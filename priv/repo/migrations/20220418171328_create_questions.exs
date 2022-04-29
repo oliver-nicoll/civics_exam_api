e0582dfcf0a2cbab7adc5a754590e5f19969b52d
Nicoll Oliver <nicolldon96@gmail.com>
2022-04-29 18:13:45 -0400

    update after restart

--- priv/repo/migrations/20220418171328_create_questions.exs
+++ priv/repo/migrations/20220418171328_create_questions.exs
@@ -5,7 +5,7 @@ defmodule CivicsExamApi.Repo.Migrations.CreateQuestions do
     create table(:questions) do
       add :title, :string
       add :description, :text
-      add :questions, :string
+      add :questions, :lists
       add :single_question, :string
       add :answers, :string
       add :correct_answer, :string
